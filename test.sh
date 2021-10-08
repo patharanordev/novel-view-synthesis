37e6ef5c1b30a034b7e119a7c64161200cf23689
Pathara Norasethasopon <patharanor@gmail.com>
2021-10-08 21:41:02 +0700

    Custom input image from 8 to 1

--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@ python2.7 evaler.py \
 --dataset chair \
 --data_id_list ./testing_tuple_lists/id_chair_random_elevation.txt \
 --checkpoint ./chair_checkpoint/model-1 \
---num_input 4 \
+--batch_size 1 \
+--num_input 1 \
 --loss True \
 --write_summary True \
 --summary_file ./output/log_chair.txt \
